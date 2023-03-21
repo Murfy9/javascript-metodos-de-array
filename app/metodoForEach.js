e474806d35f7ed620bd557db289894f471e22da6
Pedro Xavier <pedroxxavier@hotmail.com>
2023-03-21 16:06:44 -0300

    Formatando.

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,13 +1,16 @@
 const elementoParaInserirLivros = document.getElementById("livros");
-const elementoComValorTotalDeLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirOsLivrosNaTela(listaDeLivros) {
-    elementoComValorTotalDeLivrosDisponiveis.innerHTML = '';
-    elementoParaInserirLivros.innerHTML = '';
-    listaDeLivros.forEach(livro => {
-        // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
-        let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
-        elementoParaInserirLivros.innerHTML += `
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
+  elementoParaInserirLivros.innerHTML = "";
+  listaDeLivros.forEach((livro) => {
+    // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
+    elementoParaInserirLivros.innerHTML += `
         <div class="livro">
             <img class="${disponibilidade}" src="${livro.imagem}"
                 alt="${livro.alt}" />
@@ -20,8 +23,8 @@ function exibirOsLivrosNaTela(listaDeLivros) {
                 <span class="tag">${livro.categoria}</span>
             </div>
         </div>
-        `
-    });
+        `;
+  });
 }
 
 // function verificarDisponibilidadeDoLivro(livro) {
